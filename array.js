81921a3f1c219594e0bc5f15c406ce0a55d01d46
David Andersson <david.andersson@zocom.se>
2021-08-30 13:07:08 +0200

    Foreach and map

--- array.js
+++ array.js
@@ -1,1 +1,42 @@
 console.log('Array demo');
+
+const lista = ['yellow', 'white', 'lime', 'hotpink', 'black']
+
+let tenthColor = lista[9]
+console.log('The 10th color is: ', tenthColor);
+
+const xs = [1, 3, 5, 7]
+const ys = [2, 4, 6, 8]
+let zs = []
+
+for( let i=0; i<xs.length; i++ ) {
+	let x = xs[i]
+	let y = ys[i]
+	let z = x + y
+	zs.push(z)
+	console.log(` ${x} + ${y} === ${z} `);
+	// === är striktare, måste vara samma datatyp också
+}
+
+zs[15] = 11
+
+
+console.log('zs är: ', zs);
+console.log('Längden är: ', zs.length);
+console.log('Finns inte: ', zs[10]);
+
+// Higher order functions
+let sumX = 0
+xs.forEach( item => {
+	console.log('For each: ', item);
+	sumX += item
+} )
+console.log('Summa av xs: ', sumX);
+
+
+
+
+
+
+
+//
---
+++ array2.js
@@ -0,0 +1,45 @@
+const names = ['mikael', 'sixten', 'emma', 'luisa']
+
+console.log('Originalnamnen: ', names);
+
+const capitalLetters = names.map( name => {
+	return name.toUpperCase()
+} )
+console.log('Med stora bokstäver: ', capitalLetters);
+
+const capitalInitialLetter = names.map( name => {
+	let first = name.charAt(0).toUpperCase()
+	let last = name.substring(1)
+	return first + last
+	// console.log('First letter: ', first);
+	// console.log('Last: ', last);
+})
+console.log('Korrigerat: ', capitalInitialLetter);
+
+// Vi tänker oss att vi får användaruppgifter från en databas
+const users = [
+	{
+		firstName: 'Anna',
+		surName: 'Andersson',
+		email: 'anna@gmail.com'
+	},
+	{
+		firstName: 'Beata',
+		surName: 'Bengtsson',
+		email: 'beata_b@hotmail.com'
+	},
+	{
+		firstName: 'Caesar',
+		surName: 'Caspersson',
+		email: 'caesar.c@live.com'
+	}
+]
+
+const onlineUsers = users.map(user => user.firstName)
+console.log('Online users: ', onlineUsers);
+
+
+
+
+
+//
